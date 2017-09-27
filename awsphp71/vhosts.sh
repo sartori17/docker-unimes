082844ef80eb7a4922c649484748f7db7559ffa3
Felipe Sartori <felipe.sartori@unimes.br>
2017-09-27 14:44:22 -0300

    atualizacoes diversas

---
+++ awsphp71/vhosts.sh
@@ -0,0 +1,10 @@
+echo `./addvhost_docker.sh -u adminer.dev -d adminer.dev/src`
+echo `./addvhost_docker.sh -u curso_pdo.dev -d curso_pdo.dev/src`
+echo `./addvhost_docker.sh -u curso_poo.dev -d curso_poo.dev/src`
+echo `./addvhost_docker.sh -u financas.dev -d financas.dev/src`
+echo `./addvhost_docker.sh -u new_site.dev -d new_site.dev/src`
+echo `./addvhost_docker.sh -u sitesimples.dev -d sitesimples.dev/src`
+echo `./addvhost_docker.sh -u son_poo.dev -d son_poo.dev/src`
+echo `./addvhost_docker.sh -u unimes.dev -d server1`
+echo "VHOSTS criados com sucesso"
+exit 0
